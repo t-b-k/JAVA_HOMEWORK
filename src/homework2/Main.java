6914fa4e48990dee98e482c390382aceb9b33498
Tatiana <t-b-k@bk.ru>
2023-04-05 15:08:43 +0300

    Homework 2 Task 1 has finished

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -1,9 +1,18 @@
 package homework2;
 
+import homework2.tasks.BubbleSorterByIncrease;
+import homework2.tasks.MyLog;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static homework2.tasks.BubbleSorterByIncrease.*;
 import static homework2.tasks.JsonToReadableTextWriter.convertJsonToStringArray;
 
 public class Main {
+
+    private static final Logger LOG = MyLog.myLog(Main.class.getName());
     public static final int LENGTH = 10;
     public static final String JSONSTRING = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}, " +
                                             "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}, " +
@@ -13,17 +22,19 @@ public class Main {
     public static final String KEY3 = "предмет";
 
     public static void main(String[] args) {
-//        int[] sortedArray = inputArrayOfIntegersByOne(LENGTH);
+        int[] sortedArray = inputArrayOfIntegersByOne(LENGTH);
 //        System.out.println("ИСХОДНЫЙ МАССИВ: ");
 //        outputArrayOfIntToConsole(sortedArray);
-//        bubbleSortByIncrease(sortedArray);
+        LOG.log(Level.INFO, "ИСХОДНЫЙ МАССИВ: "+ Arrays.toString(sortedArray));
+        bubbleSortByIncrease(sortedArray);
 //        System.out.println("\nРЕЗУЛЬТАТ СОРТИРОВКИ: ");
 //        outputArrayOfIntToConsole(sortedArray);
+        LOG.log(Level.INFO, "Результат сортировки: "+ Arrays.toString(sortedArray));
 
-        String[] gotFromJasonString = convertJsonToStringArray(JSONSTRING);
-        for (String item: gotFromJasonString) {
-            System.out.println(item);
-        }
+//        String[] gotFromJasonString = convertJsonToStringArray(JSONSTRING);
+//        for (String item: gotFromJasonString) {
+//            System.out.println(item);
+//        }
     }
 
 
--- src/homework2/tasks/MyLog.java
+++ src/homework2/tasks/MyLog.java
@@ -8,7 +8,7 @@ public class MyLog {
         try (FileInputStream in = new FileInputStream("log.config")) {
             //полный путь до файла с конфигами
             // Файл log.config всегда должен лежать в корне проекта, то есть на одном уровне с папкой src
-            LogManager.getLogManager().readConfiguration();
+            LogManager.getLogManager().readConfiguration(in);
         } catch (Exception e) {
             e.printStackTrace();
         }
