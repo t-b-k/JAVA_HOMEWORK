f996be2b7cda9f590fb24f61d888841b1543e463
Tatiana <t-b-k@bk.ru>
2023-04-10 20:32:16 +0300

    Modified Main class

--- src/homework5/Main.java
+++ src/homework5/Main.java
@@ -1,4 +1,18 @@
 package homework5;
 
+import homework5.tasks.PhoneBookViaHashMap;
+
+import java.util.LinkedList;
+
 public class Main {
+    public static void main () {
+        String[] personPhones = new String[]{ "+7(915)055-05-73", "+7(915)055-05-69" };
+        LinkedList<String> listOfNumbers = new LinkedList<>();
+        for (String item: personPhones) {
+            listOfNumbers.add(item);
+        }
+        PhoneBookViaHashMap phoneBook = new PhoneBookViaHashMap();
+        phoneBook.addNewEntryOrPhoneNumber("Калашникова Татьяна Борисовна", listOfNumbers);
+    }
 }
+
