70e7b14f170e1211553ed0ede3c94e5d63b28794
Tatiana <t-b-k@bk.ru>
2023-04-10 23:29:02 +0300

    Adding entry into Phone book, printing phoneBook and printing an entry are added to PhoneBookViaHashMap class

--- src/homework5/Main.java
+++ src/homework5/Main.java
@@ -2,17 +2,19 @@ package homework5;
 
 import homework5.tasks.PhoneBookViaHashMap;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
-    public static void main () {
+    public static void main (String[] args) {
         String[] personPhones = new String[]{ "+7(915)055-05-73", "+7(915)055-05-69" };
-        LinkedList<String> listOfNumbers = new LinkedList<>();
+        ArrayList<String> listOfNumbers = new ArrayList<>();
         for (String item: personPhones) {
             listOfNumbers.add(item);
         }
         PhoneBookViaHashMap phoneBook = new PhoneBookViaHashMap();
         phoneBook.addNewEntryOrPhoneNumber("Калашникова Татьяна Борисовна", listOfNumbers);
+        phoneBook.printPhoneBook();
     }
 }
 
--- src/homework5/tasks/PhoneBookViaHashMap.java
+++ src/homework5/tasks/PhoneBookViaHashMap.java
@@ -1,18 +1,20 @@
 package homework5.tasks;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Set;
 
 // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 public class PhoneBookViaHashMap {
-    private static HashMap <String, LinkedList<String>> ourPhoneBook;
+    private static HashMap <String, ArrayList<String>> ourPhoneBook;
 
     public PhoneBookViaHashMap() {
         ourPhoneBook = new HashMap<>();
     }
     // Метод enqueue помещает строку в конец очереди
-    public PhoneBookViaHashMap addNewEntryOrPhoneNumber (String fio, LinkedList<String> listOfPhoneNumbers) {
-        LinkedList<String> existingPhonesList = ourPhoneBook.get(listOfPhoneNumbers);
+    public PhoneBookViaHashMap addNewEntryOrPhoneNumber (String fio, ArrayList<String> listOfPhoneNumbers) {
+        ArrayList<String> existingPhonesList = ourPhoneBook.get(fio);
         if (existingPhonesList == null) {
             ourPhoneBook.put(fio, listOfPhoneNumbers);
             // Метод возвращает
@@ -25,6 +27,34 @@ public class PhoneBookViaHashMap {
             }
             ourPhoneBook.put(fio, existingPhonesList);
             return this;
+        }
+    }
+
+    public void printPhoneBook () {
+        Set<String> setOfKeys = ourPhoneBook.keySet();
+        ArrayList<String> phoneNumbers;
+        for (String key : setOfKeys) {
+            System.out.print(key + ":");
+            phoneNumbers = ourPhoneBook.get(key);
+            int i;
+            for (i = 0; i < phoneNumbers.size() - 1; i++) {
+                System.out.print(phoneNumbers.get(i) + "; ");
+            }
+            System.out.print(phoneNumbers.get(i) + "\n");
+        }
+    }
+
+    public void printEntryByFIO (String fio) {
+//        Set<String> setOfKeys = ourPhoneBook.keySet();
+        ArrayList<String> phoneNumbers;
+//        for (String key : setOfKeys) {
+        System.out.print(fio + ":");
+        phoneNumbers = ourPhoneBook.get(fio);
+        int i;
+        for (i = 0; i < phoneNumbers.size() - 1; i++) {
+            System.out.print(phoneNumbers.get(i) + "; ");
+        }
+        System.out.print(phoneNumbers.get(i) + "\n");
         }
     }
 }
